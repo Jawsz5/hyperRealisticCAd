4ba62f2df5f4ec0562fea674870f515f09cebc99
Jazil <67172230+Jawsz5@users.noreply.github.com>
2024-04-07 20:46:22 +0000

    rocekt part

--- Rocket.py
+++ Rocket.py
@@ -4,14 +4,14 @@ import numpy as np #numeric python
 import matplotlib.pyplot as plt #graphing things with matlab
 import scipy.integrate as sci #integrating things
 
-
 ##Constant parameters
-mass = 640.0/100 ##Kg
 G = 6.6742*10**-11 #gravitational constant in SI units
 ###Planet
 rPlanet = 6357000 #kilometers
 mPlanet = 5.972e24
-#######VIDEO IS AT 3:08
+
+#Rocket parameters
+mass = 640.0/100 ##Kg
 
 #motion equations: F=ma = m*2nd derivative of altitude
 #z is the altitude above the surface
@@ -19,9 +19,23 @@ mPlanet = 5.972e24
 #zdot is the velcoity
 #z double dot is the acceleration
 #second order differential equation
-class Rockets:
 
-    
+##Gravitaitonal acceleration model
+def gravity(z):
+    global rPlanet,mPlanet
+
+    r = np.sqrt(z**2)
+
+    if r < rPlanet:
+        accel = 0.0
+    else:
+        accel = G*mPlanet/(r**3)*r
+    return accel
+
+
+
+class Rockets:
+    global mass,rPlanet,mPlanet,G
 
     def Derivatives(state, t):
         global mass
@@ -50,10 +64,12 @@ class Rockets:
 
     #### MAIN SCRIPT
 
+    print('Surface Gravity (m/s^2) = ',gravity(rPlanet))
+
     ###initial conditions
-    tZ0 = [Rplanet]
-    veloZ0 = 164.0 #m/s
-    initialState = np.array([tZ0, veloZ0])
+    z0 = rPlanet
+    veloZ0 = 25*331.0 #m/s
+    initialState = np.array([z0, veloZ0])
 
     #Time window
     tup = np.linspace(0,35,1000)
@@ -62,13 +78,14 @@ class Rockets:
     stateup = sci.odeint(Derivatives, initialState, tup)
 
     zup = stateup[:, 0]
+    altitude = zup - rPlanet
     veloZup = stateup[:, 1]
 
     ##plot this thing
 
 
     ##altitude
-    plt.plot(tup, zup)
+    plt.plot(tup, altitude)
     plt.xlabel("Time (seconds)")
     plt.ylabel("Altitude (meters)")
     plt.grid();
