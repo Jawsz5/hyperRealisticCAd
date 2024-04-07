3489b197d51ac41fea7673ea59ea8c4628997e1d
Jazil <67172230+Jawsz5@users.noreply.github.com>
2024-04-07 21:18:52 +0000

    more rocket

--- Rocket.py
+++ Rocket.py
@@ -14,23 +14,26 @@ mPlanet = 5.972e24
 mass = 640.0/100 ##Kg
 
 #motion equations: F=ma = m*2nd derivative of altitude
-#z is the altitude above the surface
+#z is the altitude from the center of the planet along the north pole
+#x is the altitude from the center along the equator
 # meters
-#zdot is the velcoity
-#z double dot is the acceleration
+#zdot is the velcoity along z
+#z double dot is the acceleration along z
 #second order differential equation
 
 ##Gravitaitonal acceleration model
-def gravity(z):
+def gravity(x, z):
     global rPlanet,mPlanet
 
-    r = np.sqrt(z**2)
+    r = np.sqrt(x**2 + z**2)
 
     if r < rPlanet:
-        accel = 0.0
+        accelX = 0.0
+        accelZ = 0.0
     else:
-        accel = G*mPlanet/(r**3)*r
-    return accel
+        accelX = G*mPlanet/(r**3)*x
+        accelZ = G*mPlanet/(r**3)*z
+    return np.asarray([accelX, accelZ])
 
 
 
@@ -40,23 +43,26 @@ class Rockets:
     def Derivatives(state, t):
         global mass
         #state vector
-        z = state[0]
-        veloZ = state[1]
+        x = state[0]
+        z = state[1]
+        veloX = state[2]
+        veloZ = state[3]
 
         #total forces: gravity, aerodynamics, thrust
-        gravityF = -gravity(z) * mass
-        aeroF = 0.0 #for now
-        thrustF = 0.0 #for now
+        gravityF = -gravity(x,z) * mass
+        aeroF = np.asarray([0.0, 0.0]) #for now
+        thrustF = np.asarray([0.0, 0.0]) #for now
         forces = gravityF + aeroF + thrustF
 
         #zdot - kinematic relationship
         zdot = veloZ
+        xdot = veloX
 
         #compute acceleration
-        zddot = forces/mass
+        ddot = forces/mass
 
         #compute the state dot vector
-        stateDot = np.asarray([zdot,zddot])
+        stateDot = np.asarray([xdot, zdot,ddot[0], ddot[1]])
 
 
 
@@ -64,22 +70,29 @@ class Rockets:
 
     #### MAIN SCRIPT
 
-    print('Surface Gravity (m/s^2) = ',gravity(rPlanet))
+    print('Surface Gravity (m/s^2) = ',gravity(0, rPlanet))
 
     ###initial conditions
-    z0 = rPlanet
-    veloZ0 = 25*331.0 #m/s
-    initialState = np.array([z0, veloZ0])
+    x0 = rPlanet + 600000
+    z0 = 00.0
+    r0 = np.sqrt(x0**2+z0**2)
+    veloZ0 = np.sqrt(G*mPlanet/r0)*1.1 #m/s
+    veloX0 = 100.0
+    initialState = np.array([x0, z0, veloX0, veloZ0])
 
     #Time window
-    tup = np.linspace(0,35,1000)
+    period = 2*np.pi/np.sqrt(G*mPlanet)*r0**(3.0/2.0)*1.5
+    tup = np.linspace(0,period,1000)
 
     #numerical integration call
     stateup = sci.odeint(Derivatives, initialState, tup)
 
-    zup = stateup[:, 0]
-    altitude = zup - rPlanet
-    veloZup = stateup[:, 1]
+    xup = stateup[:,0]
+    zup = stateup[:, 1]
+    altitude = np.sqrt(xup**2 + zup**2) - rPlanet
+    veloXup = stateup[:, 2]
+    veloZup = stateup[:, 3]
+    veloup = np.sqrt(veloXup**2 + veloZup**2)
 
     ##plot this thing
 
@@ -92,7 +105,18 @@ class Rockets:
 
     ##velo
     plt.figure(); ##new graph
-    plt.plot(tup, veloZup)
+    plt.plot(tup, veloup)
     plt.xlabel("Time (seconds)")
-    plt.ylabel("Normal Speed (meters/Second)")
+    plt.ylabel("Total speed (meters/Second)")
     plt.grid();
+
+    ##2d orbit
+    plt.figure()
+    plt.plot(xup, zup, 'r-', label='Orbit')
+    plt.plot(xup[0], zup[0], 'g*')
+    theta = np.linspace(0,2*np.pi,100)
+    xPlanet = rPlanet*np.sin(theta)
+    yPlanet = rPlanet*np.cos(theta)
+    plt.plot(xPlanet,yPlanet, 'b-', label = 'Planet')
+    plt.grid()
+    plt.legend()
